03e654c4982ca7184849056196837f819d4a019b
FedoraTipper <FedoraTipper@users.noreply.github.com>
2019-11-28 18:16:49 +0200

    init

---
+++ server/cmd/gql-server/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+    "github.com/fedoratipper/bitkiosk/server/pkg/server"
+)
+
+func main() {
+    server.Run()
+}
---
+++ server/pkg/server/main.go
@@ -0,0 +1,23 @@
+package server
+import (
+	"log"
+
+	"github.com/fedoratipper/bitkiosk/server/pkg/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+var HOST, PORT string
+
+func init() {
+	HOST = "localhost"
+	PORT = "7777"
+}
+
+// Run web server
+func Run() {
+	r := gin.Default()
+	// Setup routes
+	r.GET("/ping", handlers.Ping())
+	log.Println("Running @ http://" + HOST + ":" + PORT )
+	log.Fatalln(r.Run(HOST + ":" + PORT))
+}
